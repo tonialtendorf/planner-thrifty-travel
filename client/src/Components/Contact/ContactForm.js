516b3b4a9e685ec9974e6e92cdab9749b09b2955
tonialtendorf <tvohnoutka05@gmail.com>
2023-05-16 18:54:45 -0500

    added contact us code

---
+++ client/src/Components/Contact/ContactForm.js
@@ -0,0 +1,22 @@
+import './ContactStyle.css'
+import React from 'react'
+
+const ContactForm = () => {
+    return (
+    <div className="form">
+        <form>
+            <label>Name</label>
+            <input type="text"></input>
+            <label>Email</label>
+            <input type="text"></input>
+            <label>Activities</label>
+            <textarea rows="4" placeholder="Enter the activities you are interested in"></textarea>
+            <label>Message</label>
+            <textarea rows="6" placeholder="Type your message here"></textarea>
+            <button className="btn">Submit</button>
+        </form>
+    </div>
+)
+}
+
+export default ContactForm;
