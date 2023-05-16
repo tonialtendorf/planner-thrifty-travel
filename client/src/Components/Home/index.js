70023ba9396db429692e2d0880f107032f8a5a10
tonialtendorf <tvohnoutka05@gmail.com>
2023-05-15 20:01:29 -0500

    react file structure

---
+++ client/src/Components/Home/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Navigation from '../Navigation'
+// import Footer from '../Footer'
+// import HomeContent from '../HomeContent'
+
+const Home = () => {
+    return (
+    <div> 
+        <Navigation />
+        {/* <HomeContent />
+        <Footer /> */}
+    </div>
+    )
+}
+
+export default Home;
