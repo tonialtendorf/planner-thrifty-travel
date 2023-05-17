b83be813706db68591db6662bf38ecb112033178
tonialtendorf <tvohnoutka05@gmail.com>
2023-05-16 21:18:29 -0500

    updated footer code

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import Contact from './Components/Contact'
 import Activities from './Components/Activities';
 import Home from './Components/Home'
 import Calendar from './Components/Calendar'
-import Footer from './Components/Footer'
 import React from 'react';
 import { Route, Routes } from "react-router-dom"
 import './index.css';
@@ -17,7 +16,6 @@ function App() {
       <Route path="/activities" element={<Activities />} />
       <Route path="/calendar" element={<Calendar />} />
       <Route path="/contact" element={<Contact />} /> 
-      <Route path="/footer" element={<Footer />} />
     </Routes>
   </>
   );
