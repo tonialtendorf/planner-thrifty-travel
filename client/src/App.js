f313baa035934e0947e8381f9a6918bd21c5d0fc
jessiemullins <117420244+jessiemullins@users.noreply.github.com>
2023-05-16 20:14:15 -0400

    added ActivitiesContent

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Contact from './Components/Contact'
-import Activities from './Components/Activities';
+import Activities from './Components/Activities/ActivitiesContent';
 import Home from './Components/Home'
 import Calendar from './Components/Calendar'
 import React from 'react';
@@ -13,8 +13,8 @@ function App() {
   <>
     <Routes>
       <Route path="/" element={<Home />} />
-      {/* <Route path="/activities" element={<Activities />} />
-      <Route path="/calendar" element={<Calendar />} />*/}
+      <Route path="/activities" element={<Activities />} />
+      {/* <Route path="/calendar" element={<Calendar />} /> */}
       <Route path="/contact" element={<Contact />} /> 
     </Routes>
   </>
