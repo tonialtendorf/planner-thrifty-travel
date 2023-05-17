57119638b46659489933702d374529f3afac3a86
tonialtendorf <tvohnoutka05@gmail.com>
2023-05-16 20:00:34 -0500

    added home page images/style

--- client/src/Components/Home/index.js
+++ client/src/Components/Home/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Navigation from '../Navigation'
 // import Footer from '../Footer'
-// import HomeContent from '../HomeContent'
+import HomeContent from './HomeContent'
 
 const Home = () => {
     return (
     <div> 
         <Navigation />
-        {/* <HomeContent />
-        <Footer /> */}
+        <HomeContent />
+        {/* <Footer />  */}
     </div>
     )
 }
